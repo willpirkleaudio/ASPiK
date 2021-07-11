a647289e0827442842bc1db6ec27dd51d300a4e4
willpirkle <willpirkle@gmail.com>
2021-07-11 14:18:53 -0400

    1.6.8.004

---
+++ docs/html/struct_process_block_info.js
@@ -0,0 +1,25 @@
+var struct_process_block_info =
+[
+    [ "ProcessBlockInfo", "struct_process_block_info.html#aca21364bd9d0c02270ce267348006412", null ],
+    [ "clearMidiEvents", "struct_process_block_info.html#aad2f06452887cc3c80d26276a0e440c1", null ],
+    [ "getMidiEvent", "struct_process_block_info.html#a6c0895e55cbee18bc44292d31077aa76", null ],
+    [ "getMidiEventCount", "struct_process_block_info.html#a4d213590dd5d7676f133897120fabea8", null ],
+    [ "pushMidiEvent", "struct_process_block_info.html#af4c3704216cce276783d9c40c504bc13", null ],
+    [ "absoluteBufferTime_Sec", "struct_process_block_info.html#a92f3a1ed87f88c1f6fbc435a33c88f55", null ],
+    [ "auxInputs", "struct_process_block_info.html#a5a30bb5ef99104f7e18d1bef65622250", null ],
+    [ "auxOutputs", "struct_process_block_info.html#a380fade762a171a34daa525149c62018", null ],
+    [ "blockEndIndex", "struct_process_block_info.html#afd9bffb105baabc65d3cb8f2c5e64c89", null ],
+    [ "blockSize", "struct_process_block_info.html#a1b5024b61e65f51c2ae02b6a3471b38e", null ],
+    [ "blockStartIndex", "struct_process_block_info.html#aeb00318122b03f3187fdc3e097372e21", null ],
+    [ "BPM", "struct_process_block_info.html#a076a33b59fb9e490229fd76949b244ea", null ],
+    [ "currentBlock", "struct_process_block_info.html#a83e80ec63b733c2dd001a040c38cc95f", null ],
+    [ "inputs", "struct_process_block_info.html#a24833f51b0d750f1a76d70bdfe5baaa2", null ],
+    [ "midiEventList", "struct_process_block_info.html#ad8563abdea15b1a51498ff0eff3df77c", null ],
+    [ "numAudioInChannels", "struct_process_block_info.html#ae1bd5e678e97a38a09d55eb2d6055766", null ],
+    [ "numAudioOutChannels", "struct_process_block_info.html#a424d9a21ebeef2165af8f4006f7140dd", null ],
+    [ "numAuxAudioInChannels", "struct_process_block_info.html#a078d12da0e6558fbb7f5dcd14fbb4b0d", null ],
+    [ "numAuxAudioOutChannels", "struct_process_block_info.html#a094f61a4d2cee23b951de2244afa0521", null ],
+    [ "outputs", "struct_process_block_info.html#a3be96679452dc79a230276aa61d0e702", null ],
+    [ "timeSigDenomintor", "struct_process_block_info.html#ae4d5b482f8f63f6919fdde942daae513", null ],
+    [ "timeSigNumerator", "struct_process_block_info.html#af5cc04c285383f941cae344638a1d04d", null ]
+];
